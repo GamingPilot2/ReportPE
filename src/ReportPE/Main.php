247d9da170cbcd7ce3df5f73b6fa8adac351ee52
GannicusCoC <hugo91.dasilva@gmail.com>
2017-01-01 18:06:45 +0100

    Update Main.php

--- src/ReportPE/Main.php
+++ src/ReportPE/Main.php
@@ -2,9 +2,8 @@
 
 namespace ReportPE;
 
-use  ;
-use  ;
-use  ;
+use pocketmine\command\Command;useus
+use 
 use
 use
 use
@@ -20,6 +19,13 @@ $this->ConsoleSender->sendMessage("ReportPE are now enable !");
 public function onDisable{ 
 $this->ConsoleSender->sendMessage("ReportPE are now disabled !");
 }
-
-
-}
+  
+public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        switch($command->getName()){
+            
+          case "report"		              
+            $name = \strtolower(\array_shift($args));
+                    $player = $sender->getServer()->getPlayer($name);
+                if(!(isset($args[0]))){
+                    $sender->sendMessage(TextFormat::GREEN."Usage: /report <Player> <Reason>");
+                    return true;
