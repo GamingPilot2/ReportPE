9c8ad7dd156418108367de0931cf93ada9b98743
GamingPilot2 <louis.champi44@hotmail.com>
2017-01-01 17:13:28 +0100

    Create Main.php

---
+++ src/ReportPE/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ReportPE;
+
+use  ;
+use  ;
+use  ;
+use
+use
+use
+use
+use
+
+class Main extends PluginBase{
+
+public function onEnable{ 
+$this->ConsoleSender->sendMessage("ReportPE are now enable !");
+}
+
+public function onDisable{ 
+$this->ConsoleSender->sendMessage("ReportPE are now disabled !");
+}
+
+
+}
